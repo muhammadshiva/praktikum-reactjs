e0ca07d6ba8e44571dedeb9e951e86baeba76741
muhammadshiva <shivamuhammad07@gmail.com>
2022-02-16 09:48:23 +0700

    add notes

--- Praktikum 1 - Modern JavaScript/const.js
+++ Praktikum 1 - Modern JavaScript/const.js
@@ -1,3 +1,52 @@
-const name = 'Polinema'
+// Penggunaan Var 
 
-alert(name);
+// Scope dari var adalah global, jadi harus dibatasi menggunakan function
+
+// var buah = "apel";
+
+// Membuat fungsi namaBuah
+
+// function namaBuah() {
+//     var buah = "jeruk";
+//     console.log(buah);
+// }
+
+// Menampilkan fungsi namaBuah
+//namaBuah();
+
+// Menampilkan variable nama 
+//console.log(buah);
+
+// Penggunaan Let
+
+// Perbedaan dengan Var adalah terdapat pada Scope
+// Var cakupannya function scope
+// Let cakupannya block scope
+
+// let nama = "shiva";
+// let nama = "matahari";
+
+// console.log(nama); // Output error karena variable nama telah dideklarasi
+
+// Penggunaan Const 
+// Bersifat konstanta atau nilai tidak dapat diubah setelah variable dideklarasi
+
+// const name = 'Polinema'
+// alert(name);
+
+const mahasiswa = {
+    nama: 'boni',
+    kelas: 'TI 3Z',
+}
+
+mahasiswa.fakultas = 'informatika'
+
+console.log(mahasiswa);
+
+const mahasiswa2 = ['Boni', 'Puta'];
+
+mahasiswa2.push('Dani');
+
+console.log(mahasiswa2);
+
+
