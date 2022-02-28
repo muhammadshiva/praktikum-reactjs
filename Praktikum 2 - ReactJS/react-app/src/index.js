783196e286a3b7b4d1a33f03f19d5bc19aabb5c1
muhammadshiva <shivamuhammad07@gmail.com>
2022-02-28 12:59:26 +0700

    Praktikum 3

--- Praktikum 2 - ReactJS/react-app/src/index.js
+++ Praktikum 2 - ReactJS/react-app/src/index.js
@@ -5,18 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const Hello = () => {
-  return <p>Hello</p>
+  return <p>Hello World!</p>
 }
 
 ReactDOM.render(<Hello />, document.getElementById('root'))
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
