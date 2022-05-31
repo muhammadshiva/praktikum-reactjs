0e256ec9848dc4e1b0c76214826df3d6466a0829
muhammadshiva <shivamuhammad07@gmail.com>
2022-05-31 23:25:22 +0700

    Praktikum 12 - Firebase CRUD

---
+++ Praktikum 12 - Firebase CRUD/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9BPi69ZRneV_625MhdUiAEHQCGsODvg8",
+    authDomain: "praktikum-12-ddc52.firebaseapp.com",
+    databaseURL: "https://praktikum-12-ddc52-default-rtdb.firebaseio.com",
+    projectId: "praktikum-12-ddc52",
+    storageBucket: "praktikum-12-ddc52.appspot.com",
+    messagingSenderId: "492131937206",
+    appId: "1:492131937206:web:fec982156cbb2d9ebd6f17"
+};
+
+export default firebaseConfig
